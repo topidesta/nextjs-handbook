33b3fc8797dfc26113f3d0332b9b8c5d9afccac9
mdestafadilah <desta.08b@gmail.com>
2020-05-27 12:21:03 +0700

    Simple Bloging Static Done!

--- pages/blog.js
+++ pages/blog.js
@@ -1,3 +1,21 @@
+import Link from "next/link";
+import posts from "../data/posts.json";
+
 export default () => {
-  return <div>Blog</div>;
+  return (
+    <div>
+      <h1>Blog</h1>
+      <ul>
+        {Object.entries(posts).map((val, i) => {
+          return (
+            <li key={i}>
+              <Link href="/blog/[id]" as={"/blog/" + val[0]}>
+                <a title="judul"> {val[1].judul}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
